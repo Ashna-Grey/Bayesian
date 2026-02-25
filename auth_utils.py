0e4d6e20c0fe0da2e67d739c66278203b849b63f
Ashna Islam <ashnaislam03@gmail.com>
2026-02-25 22:37:21 +0530

    Add files via upload

---
+++ auth_utils.py
@@ -0,0 +1,11 @@
+from passlib.context import CryptContext
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+def hash_password(password: str):
+    password = password.encode("utf-8")[:72]
+    return pwd_context.hash(password)
+def verify_password(password: str, hashed: str):
+    password = password.encode("utf-8")[:72]
+    return pwd_context.verify(password, hashed)
