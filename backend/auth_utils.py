adedfc75803de7399e910a879a630e0c427db5a9
Ashna Islam <ashnaislam03@gmail.com>
2026-03-03 19:11:27 +0530

    Refactor hash and verify password functions
    Refactor password hashing functions to remove debug prints and improve password handling.

--- backend/auth_utils.py
+++ backend/auth_utils.py
@@ -1,15 +1,9 @@
 from passlib.context import CryptContext
-
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
-
 def hash_password(password: str):
-    print("PASSWORD RECEIVED:", password)
-    print("TYPE:", type(password))
-    print("LENGTH:", len(password))
-    return pwd_context.hash(password[:72])
-
+    return pwd_context.hash(password)
 def verify_password(password: str, hashed: str):
-    return pwd_context.verify(password[:72], hashed)
+    return pwd_context.verify(password, hashed)
